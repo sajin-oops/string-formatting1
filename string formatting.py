99a0592a457241179e072eb7809f296744e144a8
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-07 20:30:08 +0530

    Add files via upload

---
+++ string formatting.py
@@ -0,0 +1,9 @@
+#string formatting
+#sajin is working in google
+
+name = "sajin"
+s1 = "Working"
+company = "Google"
+text = "{} is {} in {}"
+print(text.format(name,s1,company))
+#O/P - sajin is Working in Google
